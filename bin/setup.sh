1999feb6faab4b7da69eeaddd2200b5ef9fae6cb
Jon Larrea <jon.larrea@ed.ac.uk>
2023-10-17 10:35:39 -0700

    New docker instalation

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,23 +6,11 @@ if [ "$#" -ne 3 ]; then
 fi
 
 # Install docker
-#sudo apt-get update
-#sudo apt-get install -y ca-certificates curl gnupg
-#sudo install -m 0755 -d /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#sudo chmod a+r /etc/apt/keyrings/docker.gpg
-## Add the repository to Apt sources:
-#echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get update
-#sudo apt install -y docker-ce
-
 sudo apt-get update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt install docker-ce
-sudo usermod -aG docker ${USER}
-sudo su - ${USER}
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt install -y docker-ce
 
 cd /local/repository/
 
